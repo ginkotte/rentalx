51aafa476fb95d263dd2506a59baaf12b24b9696
Gustavo Inkotte <gus.inkotte@gmail.com>
2023-02-08 16:45:45 -0300

    Categories repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,29 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { v4 as uuidV4 } from "uuid";
 
+import { CategoriesRepository } from "../repositories/CategorieRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const categoryAlredyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlredyExists) {
+    return response.status(400).json({ error: "Category alredy existd!" });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
