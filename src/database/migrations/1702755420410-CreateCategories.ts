7231232b24356c428ced9decc689f26e20c86677
Gustavo Inkotte <gus.inkotte@gmail.com>
2023-12-16 17:11:53 -0300

    postgres setup and CreateCategories migration

---
+++ src/database/migrations/1702755420410-CreateCategories.ts
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { query } from "express";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1702755420410 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
