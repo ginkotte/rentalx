24c43993edf7e297be8a2cb1b0aa05b469755018
Gustavo Inkotte <gus.inkotte@gmail.com>
2024-01-22 19:20:25 -0300

    authentication using jsonwebtoken

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or passwordn incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or passwordn incorrect!");
+    }
+
+    const token = sign({}, "6dee8ce301621e8ed25139b28a3f0af2", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
